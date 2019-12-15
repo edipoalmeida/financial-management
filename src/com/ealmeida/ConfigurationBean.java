b8ad3e41d4b71f5bb6a4361111f19416e587fe41
Edipo Almeida <almeida.edipo@icloud.com>
2019-12-15 14:30:14 +0000

    creating the repository

---
+++ src/com/ealmeida/ConfigurationBean.java
@@ -0,0 +1,13 @@
+package com.ealmeida;
+
+import javax.enterprise.context.RequestScoped;
+
+/**
+ * Class created to fix a bug (workaround) it.
+ * @author edipo
+ * @see https://bugs.eclipse.org/bugs/show_bug.cgi?id=546640
+ */
+@RequestScoped
+public class ConfigurationBean {
+
+}
